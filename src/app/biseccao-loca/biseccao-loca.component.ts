efa76f1dedeb628852d9e822e53e5806e48ba7b8
Alik Oliveira Rodrigues <alik.rodrigues@nddigital.local>
2018-06-13 09:32:38 -0300

    Biseccao acho que pronta

--- src/app/biseccao-loca/biseccao-loca.component.ts
+++ src/app/biseccao-loca/biseccao-loca.component.ts
@@ -35,7 +35,9 @@ export class BiseccaoLocaComponent implements OnInit, AfterContentInit {
       sinal2: this.formBuilder.control('-'),
       variavel2: this.formBuilder.control('1'),
       sinal3: this.formBuilder.control('+'),
+      sinal4: this.formBuilder.control('+'),
       variavel3: this.formBuilder.control('1'),
+      variavel4: this.formBuilder.control('0'),
       varA: this.formBuilder.control('', Validators.required),
       varB: this.formBuilder.control('', Validators.required),
 
@@ -54,59 +56,46 @@ export class BiseccaoLocaComponent implements OnInit, AfterContentInit {
     let expo2: number =  Number(this.formGroup.controls['expo2'].value);
     let variavel2: number = Number(this.formGroup.controls['variavel2'].value);
     let variavel3: number = Number(this.formGroup.controls['variavel3'].value);
-    let variavel4: number = Number(this.formGroup.controls['variavel4'].value);
-  
-    if (this.formGroup.controls['sinal'].value == '+' && this.formGroup.controls['sinal2'].value =='-'&&
-    this.formGroup.controls['sinal3'].value == '+') {
-    for(let i = 0; i < 8; i++) {
-      x = (Number(a) + Number(b)) / 2;
-      let result: number = Math.pow((
-        variavel * x), expo) - Math.pow((
-          variavel2 * x), expo2) - (variavel3 * x) + variavel4;
+    let variavel4: number = Number(this.formGroup.controls['sinal4'].value + this.formGroup.controls['variavel4'].value);
 
+    
+    
 
-      // let result1 = Math.pow( x, expo);
-      // let aux: number = variavel * result1;
-      // let result2 = Math.pow(x, expo2);
-      // let aux2 =  variavel2 * result2;
-      // fxk = Number((result1 * Number(resultEuler.toPrecision(6))
-      // - variavel3).toPrecision(6));
-
-      fxk = result;
+    for (let i = 0; i < 8; i++) {
+    x = (Number(a) + Number(b)) / 2;
+    
+    //fxk
+    let result: number = Number(this.formGroup.controls['sinal'].value + Math.pow((
+      variavel * x), expo)) + 
+      Number(this.formGroup.controls['sinal2'].value + Math.pow((
+        variavel2 * x), expo2)) +
+      Number(this.formGroup.controls['sinal3'].value +(
+          variavel3 * x)) + variavel4;
+      
+      fxk = Number(result.toPrecision(6));      
 
 
       //f(a)
 
-      // let aux2: number = Number(a) - variavel
-      // let result3 = Math.pow(aux2, expo);
-      // let result4 = Math.pow(Number(a), expo2) - variavel2;
-      // let resultEuler2 = Math.exp(result4)
-
-      const result1: number = Math.pow(((
-        variavel * x) * -1), expo) - Math.pow((
-          variavel2 * x), expo2) - (variavel3 * x) - variavel4;
-
+      let result1: number = Number(this.formGroup.controls['sinal'].value + Math.pow((
+        variavel * a), expo)) +
+        Number(this.formGroup.controls['sinal2'].value + Math.pow((
+          variavel2 * a), expo2)) +
+        Number(this.formGroup.controls['sinal3'].value + (
+          variavel3 * a)) + variavel4;
 
       fa = Number(result1.toPrecision(6));
-
-
-
 
       if (i == 0) {
         this.objectTable.push(new ObjectTable(i, this.formGroup.controls['varA'].value, this.formGroup.controls['varB'].value, x, fxk, fa));
       } else {
         this.objectTable.push(new ObjectTable(i, a, b, x, fxk, fa));
       }
-      if (fa * fxk  > 0) {
+      if (fa * fxk > 0) {
         a = x;
       } else {
         b = x;
       }
-    }
-
-
+    } 
   }
-  
-  }
-
 }
